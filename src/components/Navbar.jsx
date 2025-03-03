98521142e4a44b268e6e4547a19d950803b291ac
khafidzdev <khafidzrizziq@gmail.com>
2025-03-04 03:24:47 +0700

    u

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,72 +1,38 @@
-import { useState } from 'react';
-import '../styles/Navbar.css';
-import { HashLink } from 'react-router-hash-link';
-import { Link } from 'react-router-dom';
-import GradientText from "../components/GradientText/GradientText";
-function Navbar() {
-    const [menuOpen, setMenuOpen] = useState(false);
+import React from "react";
+import { VscHome, VscArchive, VscAccount, VscSettingsGear } from "react-icons/vsc"; // Import ikon dari react-icons
+import "../styles/Navbar.css"; // Import CSS
 
-    const toggleMenu = () => {
-        setMenuOpen(!menuOpen);
-    };
-
-    return (
-       <> <nav>
-       <div className="wrapper n">
-           {/* <div className="logo">
-               <a href="/">RumahRafif</a>
-           </div> */}
-
-           {/* Tombol Hamburger */}
-           <button className={`hamburger ${menuOpen ? "active" : ""}`} onClick={toggleMenu}>
-               <span></span>
-               <span></span>
-               <span></span>
-           </button>
-           </div>
-           {/* Overlay Menu */}
-           <div className={`menu ${menuOpen ? "tampil" : ""}`} onClick={toggleMenu}>
-               <ul>
-                   <li><HashLink to="/"><GradientText
-  colors={["#D100FF", " #BF00FF", "#D100FF", "#4079ff", "#40ffaa"]}
-  animationSpeed={3}
-  showBorder={false}
-  className="custom-class"
->
-  Home
-</GradientText></HashLink></li>
-                   <li><HashLink to="/#portfolio"><GradientText
-  colors={["#D100FF", " #BF00FF", "#D100FF", "#4079ff", "#40ffaa"]}
-  animationSpeed={3}
-  showBorder={false}
-  className="custom-class"
->
-  Portfolio
-</GradientText></HashLink></li>
-                   <li><HashLink to="/#about"><GradientText
-  colors={["#D100FF", " #BF00FF", "#D100FF", "#4079ff", "#40ffaa"]}
-  animationSpeed={3}
-  showBorder={false}
-  className="custom-class"
->
-  About
-</GradientText></HashLink></li>
-                   <li><HashLink to="/experience"><GradientText
-  colors={["#D100FF", " #BF00FF", "#D100FF", "#4079ff", "#40ffaa"]}
-  animationSpeed={3}
-  showBorder={false}
-  className="custom-class"
->
-  Experience
-</GradientText></HashLink></li>
-                  
-               </ul>
-               <p style={{color:'white'}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste illum architecto exercitationem quo repudiandae iure dict
-                a aut ab beatae, amet soluta saepe adipisci accusamus nam hic laudantium odit, voluptate ratione.</p>
-           </div>
-       
-   </nav></>
-    );
-}
+const Navbar = () => {
+  return (
+    <div className="dock-container">
+      <ul className="dock-menu">
+        <li>
+          <a href="/" className="dock-item">
+            <VscHome size={24} />
+            <span>Home</span>
+          </a>
+        </li>
+        <li>
+          <a href="#portfolio" className="dock-item">
+            <VscArchive size={24} />
+            <span>Portfolio</span>
+          </a>
+        </li>
+        <li>
+          <a href="#about" className="dock-item">
+            <VscAccount size={24} />
+            <span>About</span>
+          </a>
+        </li>
+        <li>
+          <a href="#settings" className="dock-item">
+            <VscSettingsGear size={24} />
+            <span>Settings</span>
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
 export default Navbar;
