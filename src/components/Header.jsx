4a9b3d695807c3c58f960283e9358a6b988fb0f7
khafidzdev <khafidzrizziq@gmail.com>
2025-03-04 16:07:01 +0700

    u

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,74 +1,36 @@
-import { useEffect } from 'react';
-import profilePicture from '../assets/profile-picture.png';
-import { FaInstagram, FaTiktok, FaTwitter, FaFacebookF } from "react-icons/fa";
-import ReactTypingEffect from 'react-typing-effect'; // Import library typing effect
-import '../styles/Header.css';
+import profilePicture from '../assets/profile-picture.png'
+import { FaInstagram } from "react-icons/fa";
+import { FaTiktok } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { FaFacebookF } from "react-icons/fa";
+import '../styles/Header.css'
 import BlurText from "../components/BlurText/BlurText";
-import TextPressure from '../components/TextPressure/TextPressure';
-import PixelTransition from '../components/PixelTransition/PixelTransition';
+import GlitchText from '../components/GlitchText/GlitchText';
 function Header() {
     const handleAnimationComplete = () => {
         console.log('Animation completed!');
       };
-      
-    useEffect(() => {
-        const createParticles = (e) => {
-            for (let i = 0; i < 3; i++) {
-                const particle = document.createElement("span");
-                particle.className = "particle";
-                document.body.appendChild(particle);
-
-                const color = "rgba(138, 43, 226, 0.8)";
-                particle.style.background = color;
-                particle.style.boxShadow = `0 0 15px ${color}`;
-
-                const size = Math.random() * 20 + 10;
-                particle.style.width = `${size}px`;
-                particle.style.height = `${size}px`;
-
-                const offsetX = (Math.random() - 0.5) * 100;
-                const offsetY = (Math.random() - 0.5) * 100;
-                particle.style.left = `${e.pageX + offsetX}px`;
-                particle.style.top = `${e.pageY + offsetY}px`;
-
-                particle.style.animation = `fadeUp 1.5s ease-out forwards`;
-
-                setTimeout(() => {
-                    particle.remove();
-                }, 1500);
-            }
-        };
-
-        document.addEventListener("mousemove", createParticles);
-        return () => document.removeEventListener("mousemove", createParticles);
-    }, []);
-
     return (
         <header>
             <div className="header-jumbotron">
-                       <div style={{position: 'relative', weight:"100%"}}>
-  <TextPressure
-    text="Khafidz Rizziq Alhuda"
-    flex={true}
-    alpha={false}
-    stroke={false}
-    width={true}
-    weight={true}
-    italic={true}
-    textColor="#ffffff"
-    strokeColor="#ff0000"
-    minFontSize={100}
-  />
-</div>
-                {/* <p>Programmer - Content Creator - Researcher</p> */}
-                <BlurText
-  text="Programmer - Content Creator - Researcher"
+                {/* <img src={profilePicture} /> */}
+                <GlitchText
+  speed={1}
+  enableShadows={true}
+  enableOnHover={true}
+  className='custom-class'
+>
+Khafidz Rizziq Alhuda
+</GlitchText>
+<BlurText
+  text="Programmer - Full Stack - Researcher"
   delay={150}
   animateBy="words"
   direction="top"
   onAnimationComplete={handleAnimationComplete}
   className="text-2xl mb-8"
 />
+                {/* <p>Programmer - Content Creator - Researcher</p> */}
                 {/* <div className='socialMedia'>
                     <a href=''><FaInstagram /></a>
                     <a href=''><FaTiktok /></a>
@@ -77,7 +39,7 @@ function Header() {
                 </div> */}
             </div>
         </header>
-    );
+    )
 }
 
-export default Header;
+export default Header
